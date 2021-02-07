59e6dd1a03a2916f854c2172d02e36ca5dd07a70
Haosik <noobbaster@gmail.com>
2021-02-07 21:51:35 +0200

    auth0 redirect urls

--- next.config.js
+++ next.config.js
@@ -9,10 +9,10 @@ module.exports = (phase, { env: parsed }) => {
       isDev,
       REDIRECT_URI: isDev
         ? 'http://localhost:3000/api/callback'
-        : 'https://example.org/api/callback',
+        : 'https://nextjs-chakra-auth0.vercel.app/api/callback',
       POST_LOGOUT_REDIRECT_URI: isDev
         ? 'http://localhost:3000/secret'
-        : 'https://example.org/secret',
+        : 'https://nextjs-chakra-auth0.vercel.app/secret',
       AUTH0_SCOPE: 'openid profile',
       SERVER_URL: isDev
         ? 'http://localhost:3000'
